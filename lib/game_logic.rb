0a44d6b63e24669cd1157558a219c60b1c45b480
sumancrest0001 <suman.crest0001@gmail.com>
2019-09-24 19:27:28 +0545

    linter error fixed

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -95,18 +95,16 @@ class UserInterface
 
   # this code gets where the user marks
   def choose_mark
-
-      puts "Player, #{game.current_player}. Please choose a box that you want to mark"
-      @game.mark = gets.chomp.to_i
-      loop do
+    puts "Player, #{game.current_player}. Please choose a box that you want to mark"
+    @game.mark = gets.chomp.to_i
+    loop do
       break if @game.cells.include? @game.mark
-      if(@game.mark >9 || @game.mark < 1)
+      if (@game.mark > 9 || @game.mark < 1)
         puts 'This is an invalid input Please select one of the rremaining cells number'
+      elsif (@game.cells.include? @game.mark) == false
+        puts 'This cell is already marked please select new one'
       end
-        if (@game.cells.include? @game.mark) == false
-          puts 'This cell is already marked please select new one'
-        end
-        @game.mark = gets.chomp.to_i
+      @game.mark = gets.chomp.to_i
     end
   end
 
