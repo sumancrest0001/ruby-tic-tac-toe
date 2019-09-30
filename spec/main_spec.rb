a1fa65070e7a4bcc706ba762d47ed2c6a89bc4ee
sumancrest0001 <suman.crest0001@gmail.com>
2019-09-30 10:36:00 +0545

    solving linter errors

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe GameLogic do
   describe '#check_empty_space method' do
     context 'when all the cells are available for marking' do
       it 'will return true' do
-        game.instance_variable_set("@cells", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+        game.instance_variable_set('@cells', [1, 2, 3, 4, 5, 6, 7, 8, 9])
         expect(game.check_empty_space). to be true
       end
     end
 
     context 'when all the cells are already marked' do
       it 'will return false' do
-        game.instance_variable_set("@cells", ["X", "O", "X", "O", "X", "O", "X", "O", "X"])
+        game.instance_variable_set('@cells', ['X', 'O', 'X', 'X', 'O', 'O','X'])
         expect(game.check_empty_space). to be false
       end
     end
@@ -31,7 +31,7 @@ RSpec.describe GameLogic do
     context 'there is no winner as well as space' do
       it 'will give true' do
         allow(game).to receive(:check_empty_space).and_return(false)
-        expect(game.game_end("X")).to be true
+        expect(game.game_end('X')).to be true
       end
     end
   end
@@ -39,12 +39,12 @@ RSpec.describe GameLogic do
   describe '#check_existance method' do
     context 'checks whether mark is already taken or not'
     it 'return true' do
-      game.instance_variable_set("@cells", [1, 2, 3, 4, 5, 6, 7, 8, 9])
+      game.instance_variable_set('@cells', [1, 2, 3, 4, 5, 6, 7, 8, 9])
       expect(game.check_existance(4)). to eq(true)
     end
 
     it 'return false as it is already marked by player X' do
-      game.instance_variable_set("@cells", [1, 2, 3, "X", "X", 6, 7, 8, 9])
+      game.instance_variable_set('@cells', [1, 2, 3, 'X', 'X', 6, 7, 8, 9])
       expect(game.check_existance(4)). to eq(false)
     end
   end
@@ -56,8 +56,8 @@ RSpec.describe Players do
   describe '#change_players method' do
     context 'this will change status of the current player' do
       it 'will return other player than current player' do
-        players.instance_variable_set("@player1", "X")
-        players.instance_variable_set("@player2", "O")
+        players.instance_variable_set('@player1', 'X')
+        players.instance_variable_set('@player2', 'O')
         @current_player = @player1
         expect(players.change_players).not_to eql(players.player2)
       end
@@ -68,8 +68,8 @@ RSpec.describe Players do
     context 'when current player is player1' do
       it 'should add the mark in the player1_array' do
         @current_player = @player1
-        players.instance_variable_set("@player1_array", [3, 5])
-        players.instance_variable_set("@mark", 7)
+        players.instance_variable_set('@player1_array', [3, 5])
+        players.instance_variable_set('@mark', 7)
         expect(players.store_mark).to eq([3, 5, 7])
       end
     end
@@ -85,10 +85,10 @@ RSpec.describe Players do
 
     context 'when array of one element has wining composition ' do
       it 'will return the player symbol' do
-        players.instance_variable_set("@player1_array", [2, 5, 7])
-        players.instance_variable_set("@player2", "O")
-        players.instance_variable_set("@player1", "X")
-        players.instance_variable_set("@player2_array", [1, 2, 3])
+        players.instance_variable_set('@player1_array', [2, 5, 7])
+        players.instance_variable_set('@player2', 'O')
+        players.instance_variable_set('@player1', 'X')
+        players.instance_variable_set('@player2_array', [1, 2, 3])
         expect(players.check_winner(compositions)).to eql(players.player2)
       end
     end
@@ -97,16 +97,16 @@ RSpec.describe Players do
   describe '#check_invalid_input method' do
     context 'when players entered a invalid input' do
       it 'will return true' do
-        players.instance_variable_set("@mark", 20)
+        players.instance_variable_set('@mark', 20)
         expect(players.check_invalid_input).to be true
       end
     end
 
-      context 'when players select a mark between 1 and 9' do
-        it 'will return false' do
-          players.instance_variable_set("@mark", 4)
-          expect(players.check_invalid_input).to be false
-        end
+    context 'when players select a mark between 1 and 9' do
+      it 'will return false' do
+        players.instance_variable_set('@mark', 4)
+        expect(players.check_invalid_input).to be false
       end
     end
   end
+end
